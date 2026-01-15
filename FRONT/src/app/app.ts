b556269f53a061f6d7f3f5d1004be8ac0f3875e3
TonNomGitHub <tonEmailAssocie@exemple.com>
2026-01-15 19:02:48 +0000

    création d'un chargeur, pour le chargement des pages, ajustement des onglets de filtre pour les petits écrans

--- FRONT/src/app/app.ts
+++ FRONT/src/app/app.ts
@@ -1,12 +1,31 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, signal, inject } from '@angular/core';
+import { RouterOutlet, Router, NavigationStart, NavigationEnd, NavigationCancel, NavigationError } from '@angular/router';
+import { LoaderComponent } from './loader/loader';
+import { LoaderService } from './services/loader.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoaderComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('GESTION-ENTREPOT');
+  private router = inject(Router);
+  private loaderService = inject(LoaderService);
+
+  constructor() {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationStart) {
+        this.loaderService.show();
+      } else if (
+        event instanceof NavigationEnd ||
+        event instanceof NavigationCancel ||
+        event instanceof NavigationError
+      ) {
+        // Petit délai pour éviter le clignotement si le chargement est trop rapide
+        setTimeout(() => this.loaderService.hide(), 500);
+      }
+    });
+  }
 }
