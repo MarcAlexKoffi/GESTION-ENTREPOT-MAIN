66f3ee91f830c1cbbe52686c41e3c03ceee4e5eb
TonNomGitHub <tonEmailAssocie@exemple.com>
2026-01-15 13:19:39 +0000

    url render configurer

--- FRONT/src/app/config.ts
+++ FRONT/src/app/config.ts
@@ -1,3 +1,10 @@
+// Détection automatique de l'environnement (Local ou Production)
+const isLocal = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
+
 export const environment = {
-  apiUrl: 'http://localhost:3000/api'
+  // En local on garde le port 3000
+  // En ligne, tu devras mettre ici l'URL que Render va te donner (ex: https://mon-app.onrender.com/api)
+  apiUrl: isLocal 
+    ? 'http://localhost:3000/api' 
+    : 'https://gestion-entrepot-main.onrender.com/api'
 };
